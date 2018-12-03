8c69e62e09eea5854c5b9e271589246a4d00c744
nkz171 <45573385+nkz171@users.noreply.github.com>
2018-12-03 20:19:01 +0100

    Add files via upload

---
+++ xlsTOcsv.ps1
@@ -0,0 +1,7 @@
+﻿$sourceFile = "sourcefile.xls"
+$targetFile = "targetfile.csv"
+$excelwb = New-Object -ComObject excel.application
+$workbook = $excelwb.Workbooks.Open($sourceFile)
+$workbook.SaveAs($targetFile,6)
+$workbook.Close($false)
+$ExcelWB.quit()
